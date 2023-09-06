7b60b8dd2af6c4a8f73ca3920ff3dcea54b20a8b
bmcafee <bennettjmcafee@gmail.com>
2023-09-06 15:06:49 -0500

    Update documentation and licensing
    Signed-off-by: bmcafee <bennettjmcafee@gmail.com>

--- R/get_data.R
+++ R/get_data.R
@@ -1,13 +1,13 @@
 #' Fetch Data from EDI
 #'
-#' Quickly download
+#' Import a data table published to EDI directly into your R environment.
 #'
-#' @param packageId A complete or partial package identifier, such as edi.200 or edi.200.12
+#' @param packageId A complete or partial package identifier, such as edi.200 or edi.200.12. Revision numbers included in this ID will overwrite the revision argument
 #' @param revision Either "newest" as a string or a numeric with a specific revision number. This will be overwritten by a revision number included in packageId
 #' @param filenum The number of the entity within the EDI data package.
-#' @param env A string describing the repository environment. Can be: "production", "staging", or "development".
+#' @param env A string describing the repository environment. Can be: "production", "staging", or "development". If you do not know what this means, keep the default value of "production"
 #'
-#' @return A dataframe of the EDI dataset
+#' @return A dataframe of the data table uploaded to EDI
 #'
 #' @export
 get_data <- function(packageId, revision = "newest", filenum = 1, env = "production") {
